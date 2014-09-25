cfe3ab4e3791e320f04796e9230120519ac707eb
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-09-25 23:02:41 +0400

    Change unfill.c behavior The program now will read array filled with numbers from stdin and turn it into partially filled array by replacing part of numbers with '?'

--- unfill.c
+++ unfill.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void printHelp(char*);
+void printUsage(char*);
 
 int main(int argc, char** argv) {
-  if (argc != 2 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+  if (argc != 3
+      || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
 #ifdef DEBUG
     printf("incorrect input: argc = %d\n", argc);
 #endif
@@ -14,16 +17,30 @@ int main(int argc, char** argv) {
   }
 
   int arraySize = atoi(argv[1]);
-  int array[arraySize][arraySize];
+  float fillingPercentage = atof(argv[2]);
 #ifdef DEBUG
-  printf("passed parameter = %d\n", arraySize);
+  printf("passed parameters = %d, %f\n", arraySize, fillingPercentage);
 #endif
 
   int number;
   int amountOfNumbers = 0;
+
+  srand(time(NULL));
+
+  float fillChance;
   while (scanf("%d", &number) != EOF) {
-    array[amountOfNumbers / arraySize][amountOfNumbers % arraySize] = number;
+    fillChance = rand() % 101 / 100.0;
+    if (fillingPercentage < 1.0 - fillChance) {
+      printf("?");
+    } else {
+      printf("%d", number);
+    }
     amountOfNumbers++;
+    if (amountOfNumbers % arraySize == 0) {
+      printf("\n");
+    } else {
+      printf("\t");
+    }
   }
 
 #ifdef DEBUG
@@ -37,7 +54,16 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+void printUsage(char* commandName) {
+  printf("Usage: %s [array size] [filling percentage]\n", commandName);
+}
+
 void printHelp(char* commandName) {
-  printf("Usage: %s [array size]\n", commandName);
+  printUsage(commandName);
+  printf("\nThis program reads input array of numbers from stdin and writes "
+      "output to stdout.\n"
+      "The output consists of elements of given array and '?' signs.\n"
+      "'?' signs are placed instead of given numbers with probability of "
+      "[filling percentage]\n");
 }
 
