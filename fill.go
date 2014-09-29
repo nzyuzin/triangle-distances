aacc1eb969794b035819cd69613e3384af8e2980
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-09-29 19:54:32 +0400

    Refactor fill.go

--- fill.go
+++ fill.go
@@ -8,49 +8,76 @@ import (
 	"strconv"
 )
 
+func printUsage(commandName string) {
+	fmt.Printf("Usage: %s [array dimension length]\n", commandName)
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		printUsage(os.Args[0])
 		os.Exit(1)
 	}
-	matrixDimension, convError := strconv.Atoi(os.Args[1])
-
-	var inputWord string
-	matrix := make([][]int, matrixDimension)
-	for i := range matrix {
-		matrix[i] = make([]int, matrixDimension)
-	}
-	amountOfNumbers := 0
+	resultDimension, convError := strconv.Atoi(os.Args[1])
 
 	if convError != nil {
 		log.Fatal(convError)
 	}
 
 	in := bufio.NewScanner(os.Stdin)
-	in.Split(bufio.ScanWords)
 
-	for in.Scan() {
-		inputWord = in.Text()
-		col := amountOfNumbers % matrixDimension
-		row := amountOfNumbers / matrixDimension
-		if inputWord == "?" {
-			matrix[row][col] = -1
+	inputNumbers := readDistancesArray(resultDimension, *in)
+	fillMissingDistances(inputNumbers)
+}
+
+func readDistancesArray(size int, input bufio.Scanner) (result [][]int) {
+	var inputDistance string
+	result = make([][]int, size)
+	for i := range result {
+		result[i] = make([]int, size)
+	}
+	amountOfNumbers := 0
+	var convError error
+
+	input.Split(bufio.ScanWords)
+
+	for input.Scan() {
+		inputDistance = input.Text()
+		col := amountOfNumbers % size
+		row := amountOfNumbers / size
+		if inputDistance == "?" {
+			result[row][col] = -1
 		} else {
-			matrix[row][col], convError = strconv.Atoi(inputWord)
+			result[row][col], convError = strconv.Atoi(inputDistance)
 		}
 		if convError != nil {
 			log.Fatal(convError)
 		}
 		amountOfNumbers++
 	}
-
-	fmt.Printf("read %d words\n", amountOfNumbers)
+	log.Printf("read %d words\n", amountOfNumbers)
+	return
 }
 
-func readInputArray() [][]int {
-	return nil
+func fillMissingDistances(distancesArray [][]int) (result [][]int) {
+	result = make([][]int, len(distancesArray))
+	for i := range result {
+		result[i] = make([]int, len(distancesArray))
+	}
+
+	for i := range distancesArray {
+		for j := range distancesArray[i] {
+			if distancesArray[i][j] == -1 {
+				calculateMissingDistance(distancesArray, i, j)
+			} else {
+				result[i][j] = distancesArray[i][j]
+			}
+		}
+	}
+
+	return
 }
 
-func printUsage(commandName string) {
-	fmt.Printf("Usage: %s [array dimension length]\n", commandName)
+func calculateMissingDistance(distancesArray [][]int, row int, column int) (result int) {
+	result = distancesArray[row][column]
+	return
 }
