ebe4066a4a644b66d6c228409386722ff6354fd7
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-10-24 00:36:23 +0400

    Fix bug when compilation error didn't interrupt test

--- test.sh
+++ test.sh
@@ -6,12 +6,12 @@ then
   exit 1
 fi
 
-make > /dev/null
 FILL_RATIO=$1
 SOURCE_FILE='testdata/normalized/1-100.txt'
 UNFILLED_FILE="testdata/unfilled/1-100-$FILL_RATIO.txt"
 GUESSED_FILE="testdata/guessed/1-100-$FILL_RATIO.txt"
 COMPARISON_FILE="testdata/compared/1-100-$FILL_RATIO.txt"
+make > /dev/null &&
 ./unfill -t -s 100 -f $FILL_RATIO < $SOURCE_FILE > $UNFILLED_FILE &&
 ./fill -d -s 100 -t < $UNFILLED_FILE > $GUESSED_FILE 2> test.fill.debug &&
 ./compare -d -t -s 100 $SOURCE_FILE < $GUESSED_FILE > $COMPARISON_FILE &&
